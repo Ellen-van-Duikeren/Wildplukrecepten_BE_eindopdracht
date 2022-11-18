81a8761688f0fe24e806e2828153aad53fc28fb0
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-11-18 16:16:03 +0100

    First commit

---
+++ src/main/java/novi/nl/wildplukrecepten/WildplukreceptenApplication.java
@@ -0,0 +1,13 @@
+package novi.nl.wildplukrecepten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildplukreceptenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildplukreceptenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/novi/nl/wildplukrecepten/WildplukreceptenApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.nl.wildplukrecepten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildplukreceptenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
